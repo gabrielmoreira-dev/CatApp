38495e8902b4d70c3b528c1b7b8cd5c28accc422
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-02-03 23:25:48 -0300

    Initial commit

---
+++ CatApp/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  CatApp
+//
+//  Created by Gabriel on 03/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
